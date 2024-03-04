01534c673645cfb86808b3972c8e4b34bd504b2d
deepak <informationtechnology20322@gmail.com>
2024-03-04 07:28:51 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
+
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+import Navbar from '@/components/Navbar'
 const inter = Inter({ subsets: ['latin'] })
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Navbar/>
+        {children}
+        <ToastContainer />
+        </body>
     </html>
   )
 }
